4b4238e2cec0a7f79727a17732923c73bfab6cc7
Julinza2007 <166446868+Julinza2007@users.noreply.github.com>
2024-05-08 11:28:15 -0300

    Agrego Avion version final de algoritmos

--- c/Algoritmos 4º2º Linzalata Julian Linzalata/Avion/Avion.cpp
+++ c/Algoritmos 4º2º Linzalata Julian Linzalata/Avion/Avion.cpp
@@ -1,26 +1,26 @@
 #include <stdio.h>
 int cliente;
-float prec_boleto, opcion_cuo, monto_total, monto_cuo, descuento;
+float prec_boleto, num_cuo, monto_total, monto_cuo, descuento;
 main(){
 	printf("Elija opciones de cliente: \n Elija opcion 1. Usted es cliente habitual. \n Elija opcion 2. Usted no es cliente habitual. \n");
 	scanf("%d", & cliente);
 	printf("Ingrese precio del boleto a pagar: ");
 	scanf("%f", & prec_boleto);
-	printf("Ingrese opcion de cuotas con sus respectivos intereses: \n Elija opcion 1 para hacer el pago con 3 cuotas con intereses del 10 porciento \n Elija opcion 2 para hacer el pago con 6 cuotas con intereses del 20 porciento \n");
-	scanf("%f", & opcion_cuo);
+	printf("Ingrese opcion de cuotas con sus respectivos intereses: \n Elija la cantidad de cuotas para hacer el pago (hasta 3 cuotas con intereses del 10 porciento), \n y (hasta 6 cuotas inclusive con intereses del 20 porciento) \n");
+	scanf("%f", & num_cuo);
 	
 	
 	
-	if (opcion_cuo == 1)
+	if (num_cuo <= 3)
 	{
 		monto_total= prec_boleto * 1.10;
-		monto_cuo= monto_total / 3;
+		monto_cuo= monto_total / num_cuo;
 		
 	}
-	else if (opcion_cuo == 2)
+	else if (num_cuo <= 6)
 	{
 		monto_total= prec_boleto * 1.2;
-		monto_cuo= monto_total / 6;
+		monto_cuo= monto_total / num_cuo;
 	}
 	
 	printf("El monto total a pagar es de: %f \n", monto_total);
